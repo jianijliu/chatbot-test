42c78f6f74bffa0f7af28cf4a88508a71ba120e6
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-06-29 15:06:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ openai.api_key = st.secrets["API_KEY"]
 def generate_response(prompt):
     st.session_state['messages'].append({"role": "user", "content": prompt})
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo"#,
-        #messages=st.session_state['messages']
+        model="gpt-3.5-turbo",
+        messages=st.session_state['messages']
     )
     response = completion.choices[0].message.content
     st.session_state['messages'].append({"role": "assistant", "content": response})
