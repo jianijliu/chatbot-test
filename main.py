d28a56bf60c2b6d2f2ea5a118cc78db45a739b1b
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-06-29 11:28:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -59,6 +59,8 @@ st.markdown('\n')
 # St the GPT-3 api key
 openai.api_key = st.secrets["API_KEY"]
 
+
+# -----------------------------------
 def generate_response(prompt):
     completions = openai.Completion.create (
         engine="text-davinci-003",
@@ -72,15 +74,38 @@ def generate_response(prompt):
     message = completions.choices[0].text
     return message
 
-st.title("AI Assistant : openAI + Streamlit")
 
-prompt = st.text_input("Enter your message:", key='prompt')
-if st.button("Submit", key='submit'):
-  response = generate_response(prompt)
-  st.success(response)
+st.title("🤖 chatBot : openAI GPT-3 + Streamlit")
 
 
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
 
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text 
+
+
+user_input = get_text()
+
+if user_input:
+    output = generate_response(user_input)
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
+
+
+# -----------------------------------
 
 ## create chatbot
 def generate_response(prompt):
