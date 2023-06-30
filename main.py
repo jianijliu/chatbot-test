860999e518a7c8c5593e7371918a66d8ca2ee353
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-06-30 21:38:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 # Display chat messages from history on app rerun
-image_url = "https://api.dicebear.com/6.x/icons/svg?icon=flower1"
+image_url = "https://api.dicebear.com/6.x/icons/svg?seed=Mimi&icon=flower1,flower3"
 for message in st.session_state.messages:
     role = message["role"]
     if role not in ["assistant"]:
