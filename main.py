3713765645203de4a5b4fefc11b949c10fbffcda
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-06-30 17:57:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ for message in st.session_state.messages:
 
 if user_id:
     # Accept user input
-    if prompt := st.chat_input("Ask ChatGPT..."):
+    if prompt := st.chat_input("Send a message"):
         input_time = str(datetime.now())
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
