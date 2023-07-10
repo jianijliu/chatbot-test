b40f79fa2cd1c4321a0fb70227ebd367651fa96a
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-07-10 15:48:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -115,5 +115,5 @@ if user_id:
 
 else:
     st.markdown("\n")
-    st.markdown("<h4 style='text-align: center;'>Please read instructions in the sidebar carefully and type in your participant ID first!</h4>", unsafe_allow_html=True)
-    st.markdown("Please read instructions in the sidebar carefully and type in your participant ID first!")
+    st.markdown("<h6 style='text-align: center;'>Please read instructions in the sidebar carefully and \n type in your participant ID first!</h6>", unsafe_allow_html=True)
+    # st.markdown("Please read instructions in the sidebar carefully and type in your participant ID first!")
