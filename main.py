72098db953395cea256de2fd976574a57301cc0b
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-07-10 15:11:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,6 @@ import socket
 #### part 0. main page setting
 st.set_page_config(page_title='Lumina.AI', page_icon=':robot:')
 st.image(image='lumina.png')
-col1, col2 = st.columns(2)
-with col1:
-    st.image(image='lumina.png')
-with col2:
-    st.header("You are chatting with Lumina.AI")
 st.write("Please paste down your participation ID in the sidebar!")
 
 #### part 1. Instruction (sidebar)
