0fc4438cdd68906da400086bd9f0264c637d800a
Jiani LIU <123154033+jianijliu@users.noreply.github.com>
2023-09-26 22:27:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ col1, col2, col3 = st.columns([1,6,1])
 with col1:
     st.write("")
 with col2:
-    st.image("logo-optima.png", width=500)   # lumina.png
+    st.image("logo-optima.png", height=200)   # lumina.png
 with col3:
     st.write("")
 # st.image(image='lumina.png', width=500)
